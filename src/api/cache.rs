3cec20809e1a63df359a89477561b927bb82bacb
GJ <deathsos@gmail.com>
2026-09-05 21:18:21 +0100

    Removed join from cache path

--- src/api/cache.rs
+++ src/api/cache.rs
@@ -1,12 +1,10 @@
 use std::{
     fs::{self, File}, io::{Read, Write}, path::PathBuf, time::{Duration, SystemTime}
 };
-use crate::{api::api_response::Rates};
+use super::api_response::Rates;
 use cache_config::CacheConfig;
 
 pub(crate) mod cache_config;
-
-const LATEST_FOLDER: &'static str = "latest";
 
 pub(crate) struct Cache {
     config: CacheConfig,
@@ -19,7 +17,7 @@ impl Cache {
 
     pub(crate) fn read_from_cache(&self, basecode: &str) -> Option<Rates> {
         let CacheConfig { folder, refresh_rate_secs: config_refresh_rate } = &self.config;
-        let cache_file = folder.join(LATEST_FOLDER).join(basecode);
+        let cache_file = folder.join(basecode);
         let mut file = match File::open(cache_file) {
             Ok(f) => f,
             Err(_) => return None,
@@ -54,9 +52,10 @@ impl Cache {
     }
 
     pub(crate) fn add_to_cache(&self, basecode: &str, data: &str) {
-        let cache_folder = PathBuf::from(&self.config.folder).join(LATEST_FOLDER);
+        let cache_folder = PathBuf::from(&self.config.folder);
         let cache_file = cache_folder.join(basecode);
         fs::create_dir_all(&cache_folder).unwrap();
+
         let mut file = File::create(&cache_file).unwrap();
         file.write_all(data.as_bytes()).unwrap();
     }
